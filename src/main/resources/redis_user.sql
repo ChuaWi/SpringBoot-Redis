caa499decbe2018672db7bbe6adac7fe32636e31
asus <2537394743@qq.com>
2020-05-27 15:53:23 +0800

    fourth commit

---
+++ src/main/resources/redis_user.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS  `redis_user`;
+CREATE TABLE `redis_user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '序号',
+  `user_id` int(10) unsigned  NOT NULL COMMENT '用户编号',
+  `user_name` varchar(25) DEFAULT NULL COMMENT '用户名',
+  `description` varchar(25) DEFAULT NULL COMMENT '描述',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+INSERT redis_user VALUES (1 ,12305,'蔡小柴','就读于国立中央大学资讯工程');
